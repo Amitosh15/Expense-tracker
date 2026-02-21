1949716cc827df04420a3f6fa89414373f5ecedc
Amitosh15 <amitosh8090@gmail.com>
2026-02-21 17:52:39 +0530

    implemented addIncome functionality

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,3 +1,27 @@
+import IncomeModel from "../models/IncomeModel.js";
+
 export const addIncome = async (req, res) => {
-  console.log(req.body);
+  const { title, amount, category, description, date } = req.body;
+
+  // Instance
+  const income = IncomeModel({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+
+  try {
+    // Validation
+    if (!title || !amount || !category || !description || !date) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+    if (amount <= 0 || amount === "number") {
+      return res.status(400).json({ message: "Amount must be a positive " });
+    }
+    await income.save();
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
