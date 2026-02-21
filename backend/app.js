d54ae184dbe62fd7fab6c4c288af5b492d324f67
Amitosh15 <amitosh8090@gmail.com>
2026-02-21 10:51:35 +0530

    connect to MongoDB Atlas via Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./db/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running at port: ${PORT}`);
