97522cddde1235f06e7cde715b56fbdd535bdd77
Amitosh15 <amitosh8090@gmail.com>
2026-02-22 16:40:17 +0530

    Implemented get delete income functionality

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -21,6 +21,33 @@ export const addIncome = async (req, res) => {
       return res.status(400).json({ message: "Amount must be a positive " });
     }
     await income.save();
+
+    res.status(201).json({ message: "Income added" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const getIncome = async (req, res) => {
+  try {
+    const incomes = await IncomeModel.find().sort({ createdAt: -1 });
+    res.status(200).json(incomes);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const deleteIncome = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const income = await IncomeModel.findByIdAndDelete(id);
+
+    if (!income) {
+      return res.status(404).json({ message: "Income not found" });
+    }
+
+    res.status(200).json({ message: "Income Deleted Successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
