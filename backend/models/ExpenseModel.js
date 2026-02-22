adbf9fc2ba1cb687d7b9a0d692266c206b83118f
Amitosh15 <amitosh8090@gmail.com>
2026-02-22 17:40:18 +0530

    created an expense model

---
+++ backend/models/ExpenseModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+// Schema
+const expenseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: "income",
+    },
+    date: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+// Model
+const ExpenseModel = mongoose.model("Expense", expenseSchema);
+export default ExpenseModel;
