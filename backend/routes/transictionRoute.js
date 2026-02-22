0b46fd2112c76d69e6b3c1bcbf820d384152fb73
Amitosh15 <amitosh8090@gmail.com>
2026-02-22 16:38:22 +0530

    Created add, get, and delete income route

--- backend/routes/transictionRoute.js
+++ backend/routes/transictionRoute.js
@@ -1,5 +1,11 @@
-import { addIncome } from "../controllers/incomeController.js";
+import {
+  addIncome,
+  deleteIncome,
+  getIncome,
+} from "../controllers/incomeController.js";
 
 export default function transictionRoute(app) {
   app.post("/api/v1/add-income", addIncome);
+  app.get("/api/v1/get-income", getIncome);
+  app.delete("/api/v1/delete-income/:id", deleteIncome);
 }
