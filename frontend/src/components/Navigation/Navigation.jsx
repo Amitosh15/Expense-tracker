b98aa103c724deb5ccbf2058cffd399d00756b5f
Amitosh15 <amitosh8090@gmail.com>
2026-02-25 22:07:21 +0530

    Remove useState & add NavLink

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,18 +2,18 @@ import { signout } from "../../utils/Icons";
 import user from "../../img/user.png";
 import { menuItems } from "../../utils/menuItem";
 import "./Navigation.css";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div
       className="navigation  w-93.5
-  bg-[#fcf6f9c7] 
-  border-[3px] border-solid border-white 
-  backdrop-blur-[4.5px] 
-  rounded-4xl 
-  flex flex-col justify-between 
-  gap-8"
+    bg-[#fcf6f9c7] 
+    border-[3px] border-solid border-white 
+    backdrop-blur-[4.5px] 
+    rounded-4xl 
+    flex flex-col justify-between 
+    gap-8"
     >
       <div className="user-container h-28 flex items-center gap-4">
         <img
@@ -29,13 +29,10 @@ const Navigation = () => {
       <ul className="menu-items flex flex-col flex-1 gap-3.5">
         {menuItems.map((item) => {
           return (
-            <li
-              key={item.id}
-              className="flex items-center gap-3 cursor-pointer text-[18px]"
-            >
+            <NavLink key={item.id} to={item.link}>
               {item.icon}
               <span>{item.title}</span>
-            </li>
+            </NavLink>
           );
         })}
       </ul>
