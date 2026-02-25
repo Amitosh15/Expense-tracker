fa2e51d3868fe6ec89e97c202641f2d5c80e1f1e
Amitosh15 <amitosh8090@gmail.com>
2026-02-25 22:02:53 +0530

    create router for components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import Transactions from "./components/Transactions/Transactions.jsx";
+import Income from "./components/Incomes/Income.jsx";
+import Expenses from "./components/Expenses/Expenses.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/transactions",
+        element: <Transactions />,
+      },
+      {
+        path: "/incomes",
+        element: <Income />,
+      },
+      {
+        path: "/expenses",
+        element: <Expenses />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRouter}></RouterProvider>,
+);
