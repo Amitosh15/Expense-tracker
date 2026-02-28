eb6dc47e523cea5521131d2c31b495ab60ea9e05
Amitosh15 <amitosh8090@gmail.com>
2026-02-28 09:36:51 +0530

    Apply icon according to category

---
+++ frontend/src/components/CategoryConfig/categoryConfig.jsx
@@ -0,0 +1,21 @@
+import {
+  bitcoin,
+  card,
+  circle,
+  freelance,
+  money,
+  piggy,
+  stocks,
+  yt,
+} from "../../utils/Icons";
+
+export const categoryConfig = {
+  salary: money,
+  freelancing: freelance,
+  investments: piggy,
+  stocks: stocks,
+  bitcoin: bitcoin,
+  bank: card,
+  youtube: yt,
+  other: circle,
+};
