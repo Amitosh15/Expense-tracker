bdcdd6a0d9ea127acca13adc566d19b7fb87e9c2
Amitosh15 <amitosh8090@gmail.com>
2026-03-03 08:43:20 +0530

    Create seperate Axios file  for API configuration

--- frontend/src/Api/Axios.jsx
+++ frontend/src/Api/Axios.jsx
@@ -4,4 +4,11 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api/v1",
 });
 
+export const addIncome = (formData) => API.post("/add-income", formData);
+export const getIncome = () => API.get("/get-income");
+export const deleteIncome = (id) => API.delete(`/delete-income/${id}`);
+export const addExpenses = (formData) => API.post("/add-expenses", formData);
+export const getExpenses = () => API.get("/get-expenses");
+export const deleteExpenses = (id) => API.delete(`/delete-expense/${id}`);
+
 export default API;
