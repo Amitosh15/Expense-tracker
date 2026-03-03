6a1a47360261eae74be454fa7770150ccbd04e82
Amitosh15 <amitosh8090@gmail.com>
2026-03-03 07:12:36 +0530

    Add icons for expenses page

--- frontend/src/components/CategoryConfig/categoryConfig.jsx
+++ frontend/src/components/CategoryConfig/categoryConfig.jsx
@@ -1,21 +1,40 @@
 import {
   bitcoin,
+  book,
   card,
   circle,
+  clothing,
+  food,
   freelance,
+  medical,
   money,
   piggy,
+  shopping,
   stocks,
+  takeaway,
+  tv,
   yt,
 } from "../../utils/Icons";
 
 export const categoryConfig = {
   salary: money,
+  shopping: shopping,
   freelancing: freelance,
   investments: piggy,
   stocks: stocks,
   bitcoin: bitcoin,
   bank: card,
   youtube: yt,
+  other: circle,
+};
+
+export const expenseCategories = {
+  education: book,
+  groceries: food,
+  health: medical,
+  subscriptions: tv,
+  takeways: takeaway,
+  clothing: clothing,
+  travelling: freelance,
   other: circle,
 };
