7d789f92f61e6b3f064d55a0c00c5342c0791a65
Amitosh15 <amitosh8090@gmail.com>
2026-03-10 10:05:30 +0530

    Add middleware

--- backend/routes/transictionRoute.js
+++ backend/routes/transictionRoute.js
@@ -8,12 +8,13 @@ import {
   deleteIncome,
   getIncome,
 } from "../controllers/incomeController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 export default function transictionRoute(app) {
-  app.post("/api/v1/add-income", addIncome);
-  app.get("/api/v1/get-income", getIncome);
-  app.delete("/api/v1/delete-income/:id", deleteIncome);
-  app.post("/api/v1/add-expenses", addExpense);
-  app.get("/api/v1/get-expenses", getExpense);
-  app.delete("/api/v1/delete-expense/:id", deleteExpense);
+  app.post("/api/v1/add-income", protect, addIncome);
+  app.get("/api/v1/get-income", protect, getIncome);
+  app.delete("/api/v1/delete-income/:id", protect, deleteIncome);
+  app.post("/api/v1/add-expenses", protect, addExpense);
+  app.get("/api/v1/get-expenses", protect, getExpense);
+  app.delete("/api/v1/delete-expense/:id", protect, deleteExpense);
 }
