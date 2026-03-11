29c33dfdfd7509fdbe530c6d78a2817f65eacc83
Amitosh15 <amitosh8090@gmail.com>
2026-03-11 06:58:37 +0530

    Implement responsive design

--- frontend/src/components/Incomes/Income.jsx
+++ frontend/src/components/Incomes/Income.jsx
@@ -1,58 +1,41 @@
 import { useEffect, useState } from "react";
 import "../../styles/global.css";
 import Form from "../IncomeForm/Form";
-import API from "../../Api/Axios";
+import { deleteIncome } from "../../Api/Axios";
 import "./Income.css";
 import { calender, comment, trash } from "../../utils/Icons";
 import { categoryConfig } from "../CategoryConfig/categoryConfig";
+import { useGlobalContext } from "../../context/GlobalContext";
 
 const Income = () => {
-  const [incomes, setIncomes] = useState([]);
+  const { incomes, getIncomes, totalIncome } = useGlobalContext();
 
-  // Get income
-  const fetchIncomes = async () => {
-    try {
-      const res = await API.get("/get-income");
-      setIncomes(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
-    fetchIncomes();
+    getIncomes();
   }, []);
 
   // Delete income
   const handleDelete = async (id) => {
     try {
-      await API.delete(`/delete-income/${id}`);
-      setIncomes(incomes.filter((income) => income._id !== id));
+      await deleteIncome(id);
+      getIncomes(); // Refresh data from context
     } catch (error) {
       console.log(error);
     }
   };
 
-  const totalIncome = () => {
-    let totalIncome = 0;
-    incomes.forEach((income) => {
-      totalIncome += income.amount;
-    });
-
-    return totalIncome;
-  };
-
   return (
-    <div className="container overflow-y-auto">
+    <div className="container">
       <h1 className="text-2xl">Incomes</h1>
-      <h2 className="total-income flex justify-center items-center bg-[#FC6F9] rounded-[20px]">
+      <h2 className="total-income flex justify-center items-center bg-[#FCF6F9] rounded-[20px]">
         Total Income: <span>₹{totalIncome()}</span>
       </h2>
       <div className="income-content flex items-start gap-4">
         {/* Form container */}
         <div className="form-container">
-          <Form fetchIncomes={fetchIncomes} />
+          <Form fetchIncomes={getIncomes} />
         </div>
-        {/* Income  */}
+        {/* Incomes */}
         <div className="incomes flex-1">
           {incomes.map((income) => {
             const { _id, title, amount, date, category, description } = income;
@@ -62,9 +45,11 @@ const Income = () => {
                 key={_id}
                 className="income-con flex items-center gap-4 bg-[#FCF6F9] rounded-[20px]"
               >
+                {/* Icon */}
                 <div className="icon w-20 h-20 rounded-[20px] bg-[#F5F5F5] flex items-center justify-center">
                   {categoryConfig[category]}
                 </div>
+                {/* Content */}
                 <div className="content flex flex-col gap-1 w-full">
                   {/* Title */}
                   <h5 className="text-[1.3rem] text-[#222260]">{title}</h5>
@@ -79,7 +64,12 @@ const Income = () => {
                     </p>
                     {/* Button */}
                     <div className="delete-btn">
-                      <button onClick={() => handleDelete(_id)}>{trash}</button>
+                      <button
+                        className="flex items-center justify-center"
+                        onClick={() => handleDelete(_id)}
+                      >
+                        {trash}
+                      </button>
                     </div>
                   </div>
                 </div>
