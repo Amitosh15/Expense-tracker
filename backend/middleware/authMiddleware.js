12c6da57f73be079c9ceeb820a536969989ca1e2
Amitosh15 <amitosh8090@gmail.com>
2026-03-13 18:12:38 +0530

    Implement JWT authentication middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,12 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  // const token = req.headers.authorization;
   const bearer = req.headers.authorization;
-
-  // if (!token) {
-  //   return res.status(401).json({ message: "Not authorized" });
-  // }
 
   if (!bearer || !bearer.startsWith("Bearer "))
     return res.status(401).json({ message: "Not authorized" });
