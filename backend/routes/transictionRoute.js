38bbed522df7d42c4ff5eb51adf4a6dd05d4d064
Amitosh15 <amitosh8090@gmail.com>
2026-03-14 08:13:01 +0530

    Add middleware in routes

--- backend/routes/transictionRoute.js
+++ backend/routes/transictionRoute.js
@@ -9,12 +9,23 @@ import {
   getIncome,
 } from "../controllers/incomeController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { validate } from "../middleware/validateMiddleware.js";
+import { addIncomeSchema } from "../validator/incomeValidator.js";
+import { addExpenseSchema } from "../validator/expenseValidator.js";
 
 export default function transictionRoute(app) {
-  app.post("/api/v1/add-income", protect, addIncome);
+  // Income
+  app.post("/api/v1/add-income", validate(addIncomeSchema), protect, addIncome);
   app.get("/api/v1/get-income", protect, getIncome);
   app.delete("/api/v1/delete-income/:id", protect, deleteIncome);
-  app.post("/api/v1/add-expenses", protect, addExpense);
+
+  // Expenses
+  app.post(
+    "/api/v1/add-expenses",
+    validate(addExpenseSchema),
+    protect,
+    addExpense,
+  );
   app.get("/api/v1/get-expenses", protect, getExpense);
   app.delete("/api/v1/delete-expense/:id", protect, deleteExpense);
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,8 @@
 import { loginUser, registerUser } from "../controllers/userController.js";
+import { validate } from "../middleware/validateMiddleware.js";
+import { signupSchema, loginSchema } from "../validator/userValidator.js";
 
 export default function userRoute(app) {
-  app.post("/api/v1/login", loginUser);
-  app.post("/api/v1/signup", registerUser);
+  app.post("/api/v1/login", validate(loginSchema), loginUser);
+  app.post("/api/v1/signup", validate(signupSchema), registerUser);
 }
