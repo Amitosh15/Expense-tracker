7d2ae16d12b1bb08a9ec9737990b025a18418e26
Amitosh15 <amitosh8090@gmail.com>
2026-03-16 09:08:36 +0530

    Add login and signup api and implement authentication interceptor

--- frontend/src/Api/Axios.jsx
+++ frontend/src/Api/Axios.jsx
@@ -4,11 +4,27 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api/v1",
 });
 
+// Request interceptor to add the token to headers
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export const addIncome = (formData) => API.post("/add-income", formData);
 export const getIncome = () => API.get("/get-income");
 export const deleteIncome = (id) => API.delete(`/delete-income/${id}`);
 export const addExpenses = (formData) => API.post("/add-expenses", formData);
 export const getExpenses = () => API.get("/get-expenses");
 export const deleteExpenses = (id) => API.delete(`/delete-expense/${id}`);
+export const registerUser = (signupInfo) => API.post("/signup", signupInfo);
+export const loginUser = (loginInfo) => API.post("/login", loginInfo);
 
 export default API;
