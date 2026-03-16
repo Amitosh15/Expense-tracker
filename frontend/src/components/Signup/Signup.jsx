476f6d18950c0c2a78973174e26eff03bef89aab
Amitosh15 <amitosh8090@gmail.com>
2026-03-16 08:38:08 +0530

    Add toast notification and tailwind styles

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { registerUser } from "../../Api/Axios";
+import { toast } from "react-toastify";
+import "../../styles/global.css";
 
 const Signup = () => {
   const [signupInfo, setSignupInfo] = useState({
@@ -16,11 +18,6 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    if (!signupInfo.name || !signupInfo.email || !signupInfo.password) {
-      alert("Please fill all required fields");
-      return;
-    }
-
     try {
       await registerUser(signupInfo);
       alert("Signup successfully");
@@ -31,49 +28,72 @@ const Signup = () => {
         password: "",
       });
     } catch (error) {
-      console.log(error);
+      if (error.response) {
+        const body = error.response.data;
+
+        if (body.error && Array.isArray(body.error)) {
+          body.errors.forEach((e) => toast.error(e.message));
+        } else if (body.message) {
+          toast.error(body.message);
+        } else {
+          toast.error("Please fill all required fields");
+        }
+      } else {
+        toast.error("Network error");
+      }
     }
   };
 
   return (
-    <div className="container">
-      <h1>Signup</h1>
-      <form onSubmit={handleSignup} className="flex flex-col items-center">
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="name"
-            placeholder="Enter your name..."
-            value={signupInfo.name}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={handleChange}
-            type="email"
-            name="email"
-            placeholder="Enter your email..."
-            value={signupInfo.email}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={handleChange}
-            type="password"
-            name="password"
-            placeholder="Enter your password..."
-            value={signupInfo.password}
-          />
-        </div>
-        <button>Signup</button>
-        <span>
-          Already have an account ? <Link to="/login">Login</Link>
-        </span>
-      </form>
+    <div className="container flex items-center justify-center">
+      <div className="signup-form bg-[#FCF6F9] border-2 flex flex-col text-center gap-5 items-center">
+        <h1 className="text-3xl">Signup</h1>
+        <form
+          onSubmit={handleSignup}
+          className="flex flex-col items-center text-left"
+        >
+          <div>
+            <label htmlFor="name">Name</label>
+            <br />
+            <input
+              onChange={handleChange}
+              type="text"
+              name="name"
+              placeholder="Enter your name..."
+              value={signupInfo.name}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              onChange={handleChange}
+              type="email"
+              name="email"
+              placeholder="Enter your email..."
+              value={signupInfo.email}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              placeholder="Enter your password..."
+              value={signupInfo.password}
+            />
+          </div>
+          <button>Signup</button>
+          <span>
+            Already have an account ?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </span>
+        </form>
+      </div>
     </div>
   );
 };
