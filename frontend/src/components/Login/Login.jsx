733ef8516c0374b2438511b4e092a05b8ced85b5
Amitosh15 <amitosh8090@gmail.com>
2026-03-16 08:30:58 +0530

    Implement react toast

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { loginUser } from "../../Api/Axios";
 import { useGlobalContext } from "../../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "../../styles/global.css";
 
 const Login = () => {
   const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
@@ -16,11 +18,6 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!loginInfo.email || !loginInfo.password) {
-      alert("Please enter email and password");
-      return;
-    }
-
     try {
       const res = await loginUser(loginInfo);
       const { token, user } = res.data; // Destructure user object
@@ -33,39 +30,52 @@ const Login = () => {
       });
       navigate("/dashboard");
     } catch (error) {
-      console.log(error);
+      if (error.response?.data?.errors) {
+        error.response.data.errors.forEach((err) => toast.error(err.message));
+      } else if (error.response?.data?.message) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Login failed");
+      }
     }
   };
 
   return (
-    <div className="container">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={handleChange}
-            type="email"
-            name="email"
-            value={loginInfo.email}
-            placeholder="Enter your email..."
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={handleChange}
-            type="password"
-            name="password"
-            value={loginInfo.password}
-            placeholder="Enter your password..."
-          />
-        </div>
-        <button>Login</button>
-        <span>
-          New user <Link to="/signup">Signup</Link>
-        </span>
-      </form>
+    <div className="container flex items-center justify-center">
+      <div className="login-form bg-[#FCF6F9] border-2 flex flex-col gap-5 items-center">
+        <h1 className="text-3xl">Login</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <div>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              onChange={handleChange}
+              type="email"
+              name="email"
+              value={loginInfo.email}
+              placeholder="Enter your email..."
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={loginInfo.password}
+              placeholder="Enter your password..."
+            />
+          </div>
+          <button>Login</button>
+          <span>
+            New user{" "}
+            <Link to="/signup" className="text-blue-600">
+              Signup
+            </Link>
+          </span>
+        </form>
+      </div>
     </div>
   );
 };
